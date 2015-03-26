e8eab6964662500c0c083c77cf94dc835073593e
vilpalu <vilius.palubinskas@ktu.edu>
2015-03-26 09:01:54 +0200

    added DDNS support
    added DDNS support for internal IP address.

---
+++ meta/openwisp-fw/files/etc/owispmanager/dnsupdate.sh
@@ -0,0 +1,13 @@
+USER="user"
+PASS="paswowd"
+IFACE="setup069"
+TMP_PATH="/tmp/"
+DNSINFO="$TMP_PATH/dns.log"
+D="$(nslookup $(cat /proc/sys/kernel/hostname).dynamic-dns.net 83.171.22.2 | head -n 5 | tail -1 | awk '{print $3}' )"
+B="$(ifconfig $IFACE | grep inet | awk '{print $2}' | cut -d':' -f2)"
+if [ $D  != $B  ]; then
+   wget -q -U "rinker.sh wget 1.0" -O $DNSINFO  "http://$USER:$PASS@nic.changeip.com/nic/update?hostname=$(cat /proc/sys/kernel/hostname).dynamic-dns.net&ip=$(ifconfig setup069 | grep inet | awk '{print $2}' | cut -d':' -f2)"
+   echo "DDNS updated"
+else
+  echo "no IP changes"
+fi
